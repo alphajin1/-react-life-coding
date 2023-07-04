5d6b96729374499d7b38f441ee5789adc4c2600b
alphajin1 <alphajin1@gmail.com>
2023-07-04 16:27:59 +0900

    React ch04 component concept

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+// example Component
+// The name of component must start with Capital.
+function Header() {
+    return <header>
+        <h1><a href="/">WEB</a></h1>
+    </header>
+}
+
+function Nav() {
+    return <nav>
+        <ol>
+            <li><a href="/read/1">html</a> </li>
+            <li><a href="/read/2">css</a> </li>
+            <li><a href="/read/3">js</a> </li>
+        </ol>
+    </nav>
+}
+
 function App() {
     return (
-        <div className="App">
-            Hello, World!
+        <div>
+            <Header></Header>
+            <Header></Header>
+            <Header></Header>
+            <Nav></Nav>
         </div>
     );
 }
